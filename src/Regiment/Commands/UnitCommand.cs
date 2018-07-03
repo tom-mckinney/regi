c10a8e9dc1d05f06ca9e664e04cdba977eeef996
Tom M <tmckinney121@gmail.com>
2018-07-03 19:12:16 -0400

    Initial commit

---
+++ src/Regiment/Commands/UnitCommand.cs
@@ -0,0 +1,14 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Regiment.Commands
+{
+    [Command("unit")]
+    public class UnitCommand
+    {
+        [Argument(0, Description = "Name of the project directory or file")]
+        public string Name { get; set; }
+    }
+}
