f6dc52e430c5a64e3d543696ebdbdf19a2ec344d
tom-mckinney <tom@elyption.com>
2018-08-15 09:34:21 -0400

    Added RunnerService

---
+++ src/Regiment.Test/Services/RunnerServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using Regiment.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace Regiment.Test.Services
+{
+    public class RunnerServiceTests
+    {
+        private readonly IRunnerService _runnerService;
+        private readonly DirectoryInfo _sampleProjectsDirectory;
+
+        public RunnerServiceTests()
+        {
+            _runnerService = new RunnerService();
+
+            _sampleProjectsDirectory = new DirectoryInfo(Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + "_SampleProjects_");
+        }
+
+        [Fact]
+        public void Throws_exception_when_directory_not_found()
+        {
+            DirectoryInfo bunkDirectory = new DirectoryInfo(Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + "BUNK_DIRECTORY");
+            Assert.Throws<DirectoryNotFoundException>(() => _runnerService.RunAsync(bunkDirectory));
+        }
+
+        [Fact]
+        public void Finds_startup_config_file_when_run_in_same_directory()
+        {
+            Assert.True(_sampleProjectsDirectory.Exists);
+        }
+    }
+}
---
+++ src/Regiment/Services/RunnerService.cs
@@ -0,0 +1,33 @@
+﻿using Regiment.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Regiment.Services
+{
+    public interface IRunnerService
+    {
+        IList<DotnetProcess> RunAsync(DirectoryInfo directory);
+        IList<DotnetProcess> RunAsync(FileInfo startupFile);
+    }
+
+    public class RunnerService : IRunnerService
+    {
+        public IList<DotnetProcess> RunAsync(DirectoryInfo directory)
+        {
+            if (!directory.Exists)
+            {
+                throw new DirectoryNotFoundException($"Could not find directory: {directory.FullName}");
+            }
+
+
+            throw new NotImplementedException();
+        }
+
+        public IList<DotnetProcess> RunAsync(FileInfo startupFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
