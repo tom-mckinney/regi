5861eff17fb565466e5ac3d56ac0576c034b5a79
Tom McKinney <tom@upstreamcode.com>
2019-12-17 23:02:59 -0500

    WIP

---
+++ src/Regi.UI/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Caliburn.Micro;
+using Regi.UI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace Regi.UI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<HomeViewModel>();
+        }
+    }
+}
---
+++ src/Regi.UI/ViewModels/HomeViewModel.cs
@@ -0,0 +1,72 @@
+﻿using Caliburn.Micro;
+using Regi.Models;
+
+namespace Regi.UI.ViewModels
+{
+    public class HomeViewModel : Screen
+    {
+		public HomeViewModel()
+		{
+			Projects.Add(new Project { Name = "Backend" });
+			Projects.Add(new Project { Name = "Frontend" });
+		}
+
+		private string _firstName;
+		public string FirstName
+		{
+			get => _firstName;
+			set
+			{
+				_firstName = value;
+				NotifyOfPropertyChange(() => FirstName);
+				NotifyOfPropertyChange(() => FullName);
+			}
+		}
+
+		private string _lastName;
+
+		public string LastName
+		{
+			get => _lastName;
+			set
+			{
+				_lastName = value;
+				NotifyOfPropertyChange(() => LastName);
+				NotifyOfPropertyChange(() => FullName);
+			}
+		}
+
+		public string FullName => $"{FirstName} {LastName}";
+
+		private BindableCollection<Project> projects = new BindableCollection<Project>();
+
+		public BindableCollection<Project> Projects
+		{
+			get => projects;
+			set
+			{
+				projects = value;
+			}
+		}
+
+		private Project _selectedProject;
+
+		public Project SelectedProject
+		{
+			get => _selectedProject;
+			set
+			{
+				_selectedProject = value;
+				NotifyOfPropertyChange(() => SelectedProject);
+			}
+		}
+
+		public bool CanClearText(string firstName, string lastName) => !string.IsNullOrEmpty(firstName) || !string.IsNullOrEmpty(lastName);
+
+		public void ClearText(string firstName, string lastName)
+		{
+			FirstName = "";
+			LastName = "";
+		}
+	}
+}
