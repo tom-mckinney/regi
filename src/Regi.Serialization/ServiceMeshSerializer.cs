73b538a76ffd6939670acdcc32ff49f63f3c54ba
Tom McKinney <tmckinney@jhancock.com>
2021-04-12 11:41:57 -0400

    Add Regi.Serialization

---
+++ src/Regi.Serialization/ServiceMeshSerializer.cs
@@ -0,0 +1,18 @@
+﻿using Regi.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regi.Serialization
+{
+    public interface IServiceMeshSerializer
+    {
+        ValueTask<IServiceMesh> 
+    }
+
+    public class ServiceMeshSerializer : IServiceMeshSerializer
+    {
+    }
+}
---
+++ test/Regi.Serialization.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Regi.Serialization.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
